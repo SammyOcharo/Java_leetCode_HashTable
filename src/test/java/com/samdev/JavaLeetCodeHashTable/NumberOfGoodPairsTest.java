6555df37c423bc36b5fd244a1be0657e9475f2d2
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-05-11 11:01:41 +0300

    leetcode problem hashtables(NUmber of Good Pairs JUnit tests)

--- src/test/java/com/samdev/JavaLeetCodeHashTable/NumberOfGoodPairsTest.java
+++ src/test/java/com/samdev/JavaLeetCodeHashTable/NumberOfGoodPairsTest.java
@@ -10,5 +10,11 @@ class NumberOfGoodPairsTest {
     void numIdenticalPairs() {
         int[] nums1 = {1, 2, 3, 1, 1, 3};
         assertEquals(4, NumberOfGoodPairs.numIdenticalPairs(nums1));
+
+        int[] nums2 = {1, 1, 1, 1};
+        assertEquals(6, NumberOfGoodPairs.numIdenticalPairs(nums2));
+
+        int[] nums3 = {1, 2, 3};
+        assertEquals(0, NumberOfGoodPairs.numIdenticalPairs(nums3));
     }
 }
